fca190d6aa3b2851c6c6f5826051baab1ba0f9a8
Narorm Mayoeurn <nmayoeurn@dminc.com>
2017-06-13 17:29:07 -0500

    Update ogm repository classes

--- ogm-datastore/src/main/java/org/bayon/ogm/datastore/DatastoreRepository.java
+++ ogm-datastore/src/main/java/org/bayon/ogm/datastore/DatastoreRepository.java
@@ -1,5 +1,10 @@
 package org.bayon.ogm.datastore;
 
+import com.google.appengine.api.datastore.Query;
+import org.bayon.ogm.datastore.query.QueryBuilder;
+
+import java.util.List;
+
 public interface DatastoreRepository<T> {
 
     T findById(Long id);
@@ -9,4 +14,12 @@ public interface DatastoreRepository<T> {
     void update(T domain);
 
     void remove(Long id);
+
+    QueryBuilder<T> createQueryBuilder();
+
+    T findOne(Query query);
+
+    List<T> find(Query query);
+
+    List<T> find(Query query, int offset, int limit);
 }
