4a65c8d4f4111eaa80b46cef6199a422dd0f6879
ChandaraLeang <leangchandara@gmail.com>
2017-06-15 15:40:08 -0500

    Create TeacherRepository, TeacherRepositoryImpl, and Update Teacher class

--- antioch-school/src/main/java/com/camhub/antiochschool/domain/Teacher.java
+++ antioch-school/src/main/java/com/camhub/antiochschool/domain/Teacher.java
@@ -11,7 +11,7 @@ public class Teacher {
     private String khmerName;
     private String englishName;
 
-    private String gender;
+    private Character gender;
     private Date birthdate;
 
 
@@ -19,4 +19,84 @@ public class Teacher {
     private Long addedby;
     private Date modifiedDate;
     private Long modifiedBy;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(String teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getKhmerName() {
+        return khmerName;
+    }
+
+    public void setKhmerName(String khmerName) {
+        this.khmerName = khmerName;
+    }
+
+    public String getEnglishName() {
+        return englishName;
+    }
+
+    public void setEnglishName(String englishName) {
+        this.englishName = englishName;
+    }
+
+    public Character getGender() {
+        return gender;
+    }
+
+    public void setGender(Character gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public Date getAddedDate() {
+        return addedDate;
+    }
+
+    public void setAddedDate(Date addedDate) {
+        this.addedDate = addedDate;
+    }
+
+    public Long getAddedby() {
+        return addedby;
+    }
+
+    public void setAddedby(Long addedby) {
+        this.addedby = addedby;
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public Long getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(Long modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
 }
---
+++ antioch-school/src/main/java/com/camhub/antiochschool/repository/TeacherRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.camhub.antiochschool.repository;
+
+import com.camhub.antiochschool.domain.Teacher;
+import org.bayon.ogm.datastore.DatastoreRepositoryAdapter;
+
+/**
+ * Created by Chandara Leang on 6/15/2017.
+ */
+public class TeacherRepositoryImpl extends DatastoreRepositoryAdapter<Teacher> implements TeacherRepository {
+}
